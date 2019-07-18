0f50d712c5c276e7c50bb469a7fd7af88eb2b9e6
Reyold Osei Adade <reynoldadade@gmail.com>
2019-07-18 10:56:39 +0000

    edited in vs

--- App1/App1/js/main.js
+++ App1/App1/js/main.js
@@ -1,3 +1,4 @@
 ﻿// Your code here!
 //extra changes will be coming soon havent figured out how to do this yet.....
 //more chages like these will be happening i am learning how to use git
+//code was added in vs
